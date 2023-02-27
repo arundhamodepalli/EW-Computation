8bf51302a076224ca725fb821f3140532bce0afe
ammu72642668 <arundhamodepalli726426@gmail.com>
2023-02-27 15:19:55 +0530

    welcome to empwage computation

---
+++ EW-Computation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EW_Computation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome employee wage computation");
+        }
+    }
+}
