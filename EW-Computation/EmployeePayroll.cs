a15439d1af1c4ec6d695049a1d78a3b7db9eafe0
ammu72642668 <arundhamodepalli726426@gmail.com>
2023-02-27 16:23:36 +0530

    CalculatecaseEmpWage in empwage computation

--- EW-Computation/EmployeePayroll.cs
+++ EW-Computation/EmployeePayroll.cs
@@ -8,30 +8,28 @@ namespace EW_Computation
 {
     public class EmployeePayroll
     {
-        public static void Parttime_EmpWage()
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+
+
+        public static void Calculatecase_EmpWage()
         {
-            //Constants
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
             //variables
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
             //Computation
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_PART_TIME)
+            switch (empCheck)
             {
-                empHrs = 4;
-            }
-            else if (empCheck == IS_FULL_TIME)
-            {
-                empHrs = 8;
-            }
-            else
-            {
-                empHrs = 0;
-            }
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+            }          
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage : " + empWage);
         }
--- EW-Computation/Program.cs
+++ EW-Computation/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             // Console.WriteLine("welcome employee wage computation");
-            EmployeePayroll.Parttime_EmpWage();
+            EmployeePayroll.Calculatecase_EmpWage();
         }
     }
 }
